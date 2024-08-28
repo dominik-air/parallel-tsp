70fd03b4e460adfc236273111b10e61b2add73a7
dominik-air <domzurek@student.agh.edu.pl>
2024-08-28 11:45:45 +0200

    time stages in runner

--- parallel_tsp/runner.py
+++ parallel_tsp/runner.py
@@ -15,6 +15,8 @@ class GeneticAlgorithmRunner:
         mpi_strategy (MPIStrategy): The MPI strategy used to parallelize the genetic algorithm.
         local_optimization_strategy (OptimizationStrategy): The local optimization strategy applied
             before running the genetic algorithm.
+        local_opt_time (float | None): The time taken to perform the local optimization, in seconds.
+        mpi_strategy_time (float): The time taken to execute the MPI strategy, in seconds.
     """
 
     def __init__(
@@ -31,6 +33,8 @@ class GeneticAlgorithmRunner:
         """
         self.mpi_strategy = mpi_strategy
         self.local_optimization_strategy = local_optimization_strategy
+        self.local_opt_time = None
+        self.mpi_strategy_time = None
 
     def run(self, comm: MPI.Comm):
         """Runs the genetic algorithm with the specified MPI and optimization strategies.
@@ -47,9 +51,12 @@ class GeneticAlgorithmRunner:
         rank = comm.Get_rank()
 
         if rank == 0:
+            start_time_local_opt = MPI.Wtime()
             optimized_population = self.local_optimization_strategy.optimize(
                 self.mpi_strategy.population
             )
+            end_time_local_opt = MPI.Wtime()
+            self.local_opt_time = end_time_local_opt - start_time_local_opt
         else:
             optimized_population = None
 
@@ -57,4 +64,22 @@ class GeneticAlgorithmRunner:
 
         self.mpi_strategy.population = optimized_population
 
-        return self.mpi_strategy.run(comm)
+        comm.Barrier()
+        start_time_mpi_strategy = MPI.Wtime()
+        best_route = self.mpi_strategy.run(comm)
+        end_time_mpi_strategy = MPI.Wtime()
+        self.mpi_strategy_time = end_time_mpi_strategy - start_time_mpi_strategy
+
+        all_local_opt_times = comm.gather(self.local_opt_time, root=0)
+        all_mpi_strategy_times = comm.gather(self.mpi_strategy_time, root=0)
+
+        if rank == 0:
+            avg_local_opt_time = (
+                sum(filter(None, all_local_opt_times)) / comm.Get_size()
+            )
+            avg_mpi_strategy_time = sum(all_mpi_strategy_times) / comm.Get_size()
+
+            print(f"Average Local Optimization Time: {avg_local_opt_time:.4f} seconds")
+            print(f"Average MPI Strategy Time: {avg_mpi_strategy_time:.4f} seconds")
+
+        return best_route
